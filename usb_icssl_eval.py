ea292996303934b12d13e62f81ae4eae4207f49f
RenpuLiu <66483750+RenpuLiu@users.noreply.github.com>
2025-05-12 09:53:49 +0800

    Update usb_icssl_eval.py

--- usb_icssl_eval.py
+++ usb_icssl_eval.py
@@ -93,9 +93,6 @@ def run_icssl_once(model_name:str,
             torch_dtype=getattr(torch, dtype),
             trust_remote_code=True)
 
-    model.generation_config.top_p        = 1.0   # equivalent to "off"
-    model.generation_config.top_k        = 0
-    model.generation_config.temperature  = 1.0
 
     tok = AutoTokenizer.from_pretrained(model_name,
                                         use_fast=True,
